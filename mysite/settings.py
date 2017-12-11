6185cbcccd64a3fa5513273dca298d67f700f450
niliel <niliel@gmail.com>
2017-12-11 12:00:29 -0300

    fixed

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9=@e6gmxz)baw&!sd8xrgiz6+k^=%vu)d)gn0dyc$f7g5sud@d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<your_username>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'niliel.pythonanywhere.com']
 
 
 # Application definition
